76535c5aa5797a79f13fc79e0e5c2ca163a84b5a
Megan Collier <meganraec@live.com>
2018-02-28 03:27:55 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ numbers = [1,2,3]
 
 def square_array(array)
   squared = Array.new(array.length)
-  array.each {|n| squared << n ** 2}
+  array.each {|n| squared << (n ** 2)}
   return squared
 end
 
